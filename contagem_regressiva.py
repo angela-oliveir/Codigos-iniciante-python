6399c534530fff78459e082992ba06b862c9c2d0
Ângela Oliveira <89156781+angela-oliveir@users.noreply.github.com>
2021-09-01 15:18:04 -0300

    Add files via upload
    Programa que faz contagem regressiva de 10 á 0 para estouro de fogos de artifício com pausa de 1 segundo entre eles.

---
+++ contagem_regressiva.py
@@ -0,0 +1,17 @@
+#Programa que faz contagem regressiva de para estouro de fogos de artifício de 10 a 0 com pausa de 1 #segundo entre eles.
+
+from time import sleep
+
+
+print('Vamos começar a contagem regressiva')
+print('⬇')
+for regressiva in range(10, 0, -1):
+    sleep(1)
+    print(regressiva)
+    
+print('🎉**🎉**🎉**🎉**🎉\n')      
+print('FELIZ ANO NOVO!\n') 
+print('🎉**🎉**🎉**🎉**🎉')      
+      
+
+
