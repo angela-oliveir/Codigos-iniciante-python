3fc46d871a25c7a31d7349f3e947903bfbe292cb
Ângela Oliveira <89156781+angela-oliveir@users.noreply.github.com>
2021-09-01 16:12:11 -0300

    Add files via upload
    Programa que mostra na tela todos os números pares que estão no intervalo de 1 e 50.

---
+++ numeros-pares.py
@@ -0,0 +1,7 @@
+#programa que mostre na tela todos os números pares que estão no intervalo de 1 e 50.
+
+
+for numero in range(2, 51, 2):
+    print(numero, end = ' . ')
+print('\nEsses são os números pares do 1 á 50')
+  
